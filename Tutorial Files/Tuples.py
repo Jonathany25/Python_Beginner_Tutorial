849347b55e1509394bf61c61fda67be52ddfc296
Jonathany25 <jyunawan@gmail.com>
2020-07-29 18:20:18 +0700

    Create Tuples.py
    Added Tuples.py

---
+++ Tutorial Files/Tuples.py
@@ -0,0 +1,8 @@
+coordinates = (4, 5)
+# Tuples are similar to lists, created by using ()
+# Tuples cannot be modified after it is made
+
+print(coordinates)
+
+print(coordinates[0])
+# can be accessed similar to lists
